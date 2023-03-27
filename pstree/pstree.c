d670c1d3d61898cfd0c67427989d113d959a5e98
junran yang <usbskycrafts@outlook.com>
2023-03-27 22:38:11 +0000

    new file:   .shadow/abstract-machine/.gitignore 	new file:   .shadow/abstract-machine/LICENSE 	new file:   .shadow/abstract-machine/Makefile 	new file:   .shadow/abstract-machine/README 	new file:   .shadow/abstract-machine/am/Makefile 	new file:   .shadow/abstract-machine/am/include/am.h 	new file:   .shadow/abstract-machine/am/include/amdev.h 	new file:   .shadow/abstract-machine/am/include/arch/mips32-nemu.h 	new file:   .shadow/abstract-machine/am/include/arch/native.h 	new file:   .shadow/abstract-machine/am/include/arch/riscv32-nemu.h 	new file:   .shadow/abstract-machine/am/include/arch/riscv64-mycpu.h 	new file:   .shadow/abstract-machine/am/include/arch/riscv64-nemu.h 	new file:   .shadow/abstract-machine/am/include/arch/spike.h 	new file:   .shadow/abstract-machine/am/include/arch/x86-nemu.h 	new file:   .shadow/abstract-machine/am/include/arch/x86-qemu.h 	new file:   .shadow/abstract-machine/am/include/arch/x86_64-qemu.h 	new file:   .shadow/abstract-machine/am/src/native/cte.c 	new file:   .shadow/abstract-machine/am/src/native/ioe.c 	new file:   .shadow/abstract-machine/am/src/native/ioe/audio.c 	new file:   .shadow/abstract-machine/am/src/native/ioe/disk.c 	new file:   .shadow/abstract-machine/am/src/native/ioe/gpu.c 	new file:   .shadow/abstract-machine/am/src/native/ioe/input.c 	new file:   .shadow/abstract-machine/am/src/native/ioe/timer.c 	new file:   .shadow/abstract-machine/am/src/native/mpe.c 	new file:   .shadow/abstract-machine/am/src/native/platform.c 	new file:   .shadow/abstract-machine/am/src/native/platform.h 	new file:   .shadow/abstract-machine/am/src/native/trap.S 	new file:   .shadow/abstract-machine/am/src/native/trm.c 	new file:   .shadow/abstract-machine/am/src/native/vme.c 	new file:   .shadow/abstract-machine/am/src/x86/nemu/cte.c 	new file:   .shadow/abstract-machine/am/src/x86/nemu/start.S 	new file:   .shadow/abstract-machine/am/src/x86/nemu/trap.S 	new file:   .shadow/abstract-machine/am/src/x86/nemu/vme.c 	new file:   .shadow/abstract-machine/am/src/x86/qemu/boot/Makefile 	new file:   .shadow/abstract-machine/am/src/x86/qemu/boot/genboot.py 	new file:   .shadow/abstract-machine/am/src/x86/qemu/boot/main.c 	new file:   .shadow/abstract-machine/am/src/x86/qemu/boot/start.S 	new file:   .shadow/abstract-machine/am/src/x86/qemu/cte.c 	new file:   .shadow/abstract-machine/am/src/x86/qemu/ioe.c 	new file:   .shadow/abstract-machine/am/src/x86/qemu/mpe.c 	new file:   .shadow/abstract-machine/am/src/x86/qemu/start32.S 	new file:   .shadow/abstract-machine/am/src/x86/qemu/start64.S 	new file:   .shadow/abstract-machine/am/src/x86/qemu/trap32.S 	new file:   .shadow/abstract-machine/am/src/x86/qemu/trap64.S 	new file:   .shadow/abstract-machine/am/src/x86/qemu/trm.c 	new file:   .shadow/abstract-machine/am/src/x86/qemu/vme.c 	new file:   .shadow/abstract-machine/am/src/x86/qemu/x86-qemu.h 	new file:   .shadow/abstract-machine/am/src/x86/x86.h 	new file:   .shadow/abstract-machine/klib/Makefile 	new file:   .shadow/abstract-machine/klib/include/klib-macros.h 	new file:   .shadow/abstract-machine/klib/include/klib.h 	new file:   .shadow/abstract-machine/klib/src/cpp.c 	new file:   .shadow/abstract-machine/klib/src/int64.c 	new file:   .shadow/abstract-machine/klib/src/stdio.c 	new file:   .shadow/abstract-machine/klib/src/stdlib.c 	new file:   .shadow/abstract-machine/klib/src/string.c 	new file:   .shadow/abstract-machine/scripts/isa/x86.mk 	new file:   .shadow/abstract-machine/scripts/isa/x86_64.mk 	new file:   .shadow/abstract-machine/scripts/native.mk 	new file:   .shadow/abstract-machine/scripts/platform/qemu.mk 	new file:   .shadow/abstract-machine/scripts/x86-qemu.mk 	new file:   .shadow/abstract-machine/scripts/x86_64-qemu.mk 	new file:   .shadow/pstree/pstree.c 	modified:   pstree/pstree.c

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,11 +1,47 @@
-#include <stdio.h>
 #include <assert.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#define print(format, ...) fprintf(stderr, format, ##__VA_ARGS__)
 
 int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
+  struct option long_options[] = {
+      {"show-pids", optional_argument, NULL, 'p'},
+      {"numeric-sort", optional_argument, NULL, 'n'},
+      {"version", optional_argument, NULL, 'V'},
+      {0, 0, 0, 0}};
+  const char *option = "pnV";
+  int option_index = 0;
+  bool sorted = false;
+  int arg;
+  while ((arg = getopt_long(argc, argv, option, long_options, &option_index)) !=
+         -1) {
+    switch (arg) {
+    case 'p': {
+      sorted = true;
+      break;
+    }
+    case 'n': {
+
+      break;
+    }
+    case 'V': {
+      print("my pstree, version 0.1\n");
+      fflush(stderr);
+      break;
+    }
+    default: {
+      print("unkown args, check again\n");
+      fflush(stderr);
+      assert(0);
+    };
+    }
   }
-  assert(!argv[argc]);
+  // get info in /proc/[pid]/stat
+  if(sorted) {
+
+  }
   return 0;
 }
